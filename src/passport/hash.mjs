00032a94a475e06e1abe3f19649635c6b2835510
himxa <himxa0x@gmail.com>
2025-12-11 10:53:57 +0100

    coverd hashing password

---
+++ src/passport/hash.mjs
@@ -0,0 +1,16 @@
+import bcrypt from 'bcrypt';
+
+
+const saltRounds = 10;
+
+export const hasPassword = async (password)=>{
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
+}
+
+
+
+export const comparePassword = async (password, hashedPassword)=>{
+    return await bcrypt.compare(password, hashedPassword);
+}
